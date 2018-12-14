01830deed6055617d2cab560aea9ae54d63e5cc9
Artur Wozniak <wozniaka83@gmail.com>
2018-12-14 07:35:33 +0100

    refactor

--- wozniak_z1.cpp
+++ wozniak_z1.cpp
@@ -1,3 +1,18 @@
+
+/* 1. wczytuje imię użytkownika (jako zmienną typu string).
+2. wczytuje jeden znak (zmienną typu char) informujący, czy użytkownik jest kobietą, czy
+mężczyzną (należy podać dużą lub małą literę K lub M).
+3. jeśli dane o płci zostały poprawnie podane, to drukuje powitanie z użyciem imienia w
+postaci: imie, witam Pania/Pana (imion nie trzeba odmieniać), np. Andrzej, witam Pana.
+W przypadku danych niepoprawnych drukuje powitanie postaci Witaj, imie.
+4. wczytuje dwie dowolne liczby całkowite (mogą być ujemne) do zmiennych typu int.
+5. drukuje następujące informacje słowne o większej z wczytanych liczb:
+– czy jest parzysta czy nieparzysta
+– czy jest jednocyfrowa, czy dwucyfrowa, czy też inna.
+6. drukuje wynik dzielenia pierwszej liczby przez podwojoną wartość drugiej liczby.
+Wskazówka: nie wolno dopuścić do dzielenia przez zero (wydrukować odpowiedni komunikat) i do obcinania
+wyniku po przecinku – wynik musi być dokładny. */
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -8,34 +23,24 @@ int main(int argc, char *argv[])
 {
   string name;
   char gender;
-  int num_1, num_2;
-  float score, num_1_equ, num_2_equ;
-
+  int num_1, num_2, bigger;
+  //float score, num_1_equ, num_2_equ;
 
   cout << "Podaj imie" << endl;
   cin >> name;
-  cout << "Witaj: " << name << endl;
 
   cout << "Jestes M (mezczyzna) czy K (kobieta)" << endl;
   cin >> gender;
-  cout <<"Jestes " << gender << endl;
 
   if (gender == 'M' || gender == 'm')
   {
     cout << name << " , witam Pana" << endl;
   }
-
   else if (gender == 'K' || gender == 'k')
   {
     cout << name << " , witam Pania" << endl;
   }
-
-  else if (gender != 'K' || gender != 'M')
-  {
-    cout << "Witaj, " << name << endl;
-  }
-
-  else if (gender != 'k' || gender != 'm')
+  else
   {
     cout << "Witaj, " << name << endl;
   }
@@ -43,98 +48,29 @@ int main(int argc, char *argv[])
   cout << "Podaj dwie liczby" << endl;
   cin >> num_1 >> num_2;
 
-  if (num_1 == 0 || num_2 == 0)
+  if (num_1 > num_2 )
   {
-    cout << "Liczby musza byc wieksze od 0, " << name << " sprobuj jeszcze raz" << endl;
-    system("PAUSE");
-  }
-
-  else if (num_1 > num_2)
-  {
-    if (num_1 % 2 == 0 && num_1 > 9)
-    {
-      cout << "Pierwsza liczba jest wieksza, jest parzysta i przynajmniej dwucyfrowa" << endl;
-    }
-
-    else if (num_1 % 2 == 0 && num_1 <= 9)
-    {
-      cout << "Pierwsza liczba jest wieksza, jest parzysta i NIE jest dwucyfrowa" << endl;
-
-      num_1_equ = num_1;
-      num_2_equ = num_2;
-    }
-
-    else if (num_1 % 2 != 0 && num_1 > 9)
-    {
-      cout << "Pierwsza liczba jest wieksza, NIE jest parzysta, jest przynajmniej dwucyfrowa" << endl;
-
-      num_1_equ = num_1;
-      num_2_equ = num_2;
-    }
-
-    else if (num_1 % 2 != 0 && num_1 <= 9)
-    {
-      cout << "Pierwsza liczba jest wieksza, NIE jest parzysta, Nie jest dwucyfrowa" << endl;
-
-      num_1_equ = num_1;
-      num_2_equ = num_2;
-    }
+    bigger = num_1;
   }
 
   else if (num_1 < num_2)
   {
-    if (num_2 % 2 == 0 && num_2 > 9)
-    {
-      cout << "Druga liczba jest wieksza, jest parzysta i przynajmniej dwucyfrowa" << endl;
-
-      num_1_equ = num_1;
-      num_2_equ = num_2;
-    }
-
-    else if (num_2 % 2 == 0 && num_2 <= 9)
-    {
-      cout << "Druga liczba jest wieksza, jest parzysta i NIE jest dwucyfrowa" << endl;
-
-      num_1_equ = num_1;
-      num_2_equ = num_2;
-    }
-
-    else if (num_2 % 2 != 0 && num_2 > 9)
-    {
-      cout << "Druga liczba jest wieksza, NIE jest parzysta, jest przynajmniej dwucyfrowa" << endl;
-
-      num_1_equ = num_1;
-      num_2_equ = num_2;
-    }
-
-    else if (num_2 % 2 != 0 && num_2 <= 9)
-    {
-      cout << "Druga liczba jest wieksza, NIE jest parzysta, Nie jest dwucyfrowa" << endl;
-
-      num_1_equ = num_1;
-      num_2_equ = num_2;
-    }
+    bigger = num_2;
   }
-
-  else if (num_1 == num_2 && num_1 % 2 == 0 && num_2 % 2 == 0)
-  {
-    cout <<"Podane liczby sa rowne i sa PARZYSTE" << endl;
-
-    num_1_equ = num_1;
-    num_2_equ = num_2;
-  }
-
   else
   {
-    cout <<"Podane liczby sa rowne i sa NIE-PARZYSTE" << endl;
-
-    num_1_equ = num_1;
-    num_2_equ = num_2;
+    cout <<"Podane liczby sa rowne" << endl;
   }
 
-  score = num_1_equ / (num_2_equ * 2);
+  if (bigger % 2 == 0) {
+    cout << "Wieksza liczba jest parzysta" << endl;
+  }else
+  {
+    cout << "Wieksza liczba jest nieparzysta" << endl;
+  }
+  //score = num_1_equ / (num_2_equ * 2);
 
-  cout << score << endl;
+  //cout << score << endl;
 
   system("pause");
 
