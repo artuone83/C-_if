5187c3a29d440629abcef7d12d33ad0b769649ba
Artur Wozniak <wozniaka83@gmail.com>
2018-12-16 12:38:18 +0100

    beta

--- wozniak_z1.cpp
+++ wozniak_z1.cpp
@@ -1,18 +1,3 @@
-
-/* 1. wczytuje imię użytkownika (jako zmienną typu string).
-2. wczytuje jeden znak (zmienną typu char) informujący, czy użytkownik jest kobietą, czy
-mężczyzną (należy podać dużą lub małą literę K lub M).
-3. jeśli dane o płci zostały poprawnie podane, to drukuje powitanie z użyciem imienia w
-postaci: imie, witam Pania/Pana (imion nie trzeba odmieniać), np. Andrzej, witam Pana.
-W przypadku danych niepoprawnych drukuje powitanie postaci Witaj, imie.
-4. wczytuje dwie dowolne liczby całkowite (mogą być ujemne) do zmiennych typu int.
-5. drukuje następujące informacje słowne o większej z wczytanych liczb:
-– czy jest parzysta czy nieparzysta
-– czy jest jednocyfrowa, czy dwucyfrowa, czy też inna.
-6. drukuje wynik dzielenia pierwszej liczby przez podwojoną wartość drugiej liczby.
-Wskazówka: nie wolno dopuścić do dzielenia przez zero (wydrukować odpowiedni komunikat) i do obcinania
-wyniku po przecinku – wynik musi być dokładny. */
-
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -97,48 +82,49 @@ int main(int argc, char *argv[])
     bigger = num_2;
     if (num_2 == 0)
     {
-      cout << "Pamietaj cholero nie dziel przez 0" << endl;
+      cout << "Nie dzielimy przez 0" << endl;
       system("PAUSE");
-    }else
-    {
-      if (bigger % 2 == 0)
-    {
-      cout << "Wieksza liczba jest parzysta" << endl;
-      if (bigger < 10 && bigger > -10)
-      {
-        cout << "Wieksza liczba jest jednocyfrowa" << endl;
-      }
-      else if (bigger > 10 && bigger < 100)
-      {
-        cout << "Wieksza liczba jest dwucyfrowa" << endl;
-      }
-      else if (bigger < -9 && bigger > -100)
-      {
-        cout << "Wieksza liczba jest dwucyfrowa" << endl;
-      }
-      else
-      {
-        cout << "Wieksza liczba jest inna" << endl;
-      }
     }
     else
     {
-      cout << "Wieksza liczba jest nieparzysta" << endl;
-      if (bigger < 10 && bigger > -10)
+      if (bigger % 2 == 0)
       {
-        cout << "Wieksza liczba jest jednocyfrowa" << endl;
-      }else if (bigger > 10 && bigger < 100)
-      {
-      cout << "Wieksza liczba jest dwucyfrowa" << endl;
-      }else if (bigger < -9 && bigger > -100)
-      {
-        cout << "Wieksza liczba jest dwucyfrowa" << endl;
+        cout << "Wieksza liczba jest parzysta" << endl;
+        if (bigger < 10 && bigger > -10)
+        {
+          cout << "Wieksza liczba jest jednocyfrowa" << endl;
+        }
+        else if (bigger > 10 && bigger < 100)
+        {
+          cout << "Wieksza liczba jest dwucyfrowa" << endl;
+        }
+        else if (bigger < -9 && bigger > -100)
+        {
+          cout << "Wieksza liczba jest dwucyfrowa" << endl;
+        }
+        else
+        {
+          cout << "Wieksza liczba jest inna" << endl;
+        }
       }
       else
       {
-        cout << "Wieksza liczba jest inna" << endl;
+        cout << "Wieksza liczba jest nieparzysta" << endl;
+        if (bigger < 10 && bigger > -10)
+        {
+          cout << "Wieksza liczba jest jednocyfrowa" << endl;
+        }else if (bigger > 10 && bigger < 100)
+        {
+        cout << "Wieksza liczba jest dwucyfrowa" << endl;
+        }else if (bigger < -9 && bigger > -100)
+        {
+          cout << "Wieksza liczba jest dwucyfrowa" << endl;
+        }
+        else
+        {
+          cout << "Wieksza liczba jest inna" << endl;
+        }
       }
-    }
     }
   }
 
@@ -153,4 +139,3 @@ int main(int argc, char *argv[])
 
   return 0;
 }
-
